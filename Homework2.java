5b9d539435d553f1d675c6ece0a1fbe978c850fd
Mircea <105327253+MirceaGutium@users.noreply.github.com>
2022-05-13 00:52:52 +0300

    Add files via upload

---
+++ Homework2.java
@@ -0,0 +1,9 @@
+package Homework2_2;
+
+public class Homework2 {
+        public static void main(String[] args) {
+            int a = 512343;
+            int b = 3431231;
+            System.out.println(a + b);
+        }
+    }
